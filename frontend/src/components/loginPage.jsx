4ad9946caa9e0cd7cadbd272562fb31ad9671cff
reddy-gopal <gopalreddyanthapu07@gmail.com>
2025-05-27 09:46:59 +0530

    Update loginPage.jsx

--- frontend/src/components/loginPage.jsx
+++ frontend/src/components/loginPage.jsx
@@ -140,7 +140,7 @@ const Register = () => {
     }
 
     try {
-      const response = await fetch('http://127.0.0.1:8000/api/register/', {
+      const response = await fetch('https://gopal955.pythonanywhere.com/api/register/', {
         method: 'POST',
         headers: { 
           'Content-Type': 'application/json',
